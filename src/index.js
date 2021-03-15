e993568b912f56da5de95285ca2ff0e885d5529b
NikitaMih <70882448+NikitaMih@users.noreply.github.com>
2021-03-15 22:56:33 +0300

    b

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 module.exports = function reverse (n) {
-   var digit, result = 0
-
-    while( n ){
-        digit = n % 10  //  Get right-most digit. Ex. 123/10 → 12.3 → 3
-        result = (result * 10) + digit  //  Ex. 123 → 1230 + 4 → 1234
-        n = n/10|0  //  Remove right-most digit. Ex. 123 → 12.3 → 12
-    }  
-  
-    return result
-}
+  let val = n.toString().split('').reverse().join('');
+  if (val.endsWith('-')) {
+    val = '-' + val;
+      return parseInt(val);
+  }
+      return parseInt(val);
 }
